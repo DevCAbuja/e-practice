1b6a42b1ad28114a2af205905bc88a830f550fa9
?ranky <sammysmart95@gmail.com>
2019-07-25 21:21:40 +0100

    add logo carousel to the login page

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import DevC from '../../asset/imgs/devc.png';
+import DevC1 from '../../asset/imgs/devc1.png';
+import DevC2 from '../../asset/imgs/devc2.png';
+import DevC3 from '../../asset/imgs/devc3.png';
 import UserNameIcon from '../../asset/icons/username.png';
 import PasswordIcon from '../../asset/icons/password.png';
 
+
 class Login extends Component {
   render() {
-    return (      
+    return (
       <div className="login">
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="d-block w-100" src={DevC} alt="First slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src={DevC} alt="Second slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src={DevC} alt="Third slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src={DevC} alt="Forth slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src={DevC} alt="Fifth slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src={DevC} alt="Sixth slide" />
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>          
+          <ol className="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+          </ol>
+        </div>    
         <div className="column1" />
         <div className="box">
           <div className="row">
